60a3f79f7e9bd1a4ca501e4118d477860c5c6c33
Actor CMS <actor@localhost>
2026-09-06 23:16:57 +0000

    deploy: 2026-09-06T23:16:57.219Z

--- js/data-comments.js
+++ js/data-comments.js
@@ -1,32 +1,3 @@
 window.SITE_CONFIG = window.SITE_CONFIG || {};
-window.SITE_CONFIG.comments = {
-    "album-a-IyyB3Q": [
-        {
-            "id": "c-mtpvpnkk",
-            "n": "杉果派",
-            "t": "测试评论：评论功能上线验证～",
-            "d": "2026-09-06"
-        },
-        {
-            "id": "i-97",
-            "n": "马铃薯",
-            "t": "留言测试",
-            "d": "2026-09-06"
-        },
-        {
-            "id": "i-98",
-            "n": "杉果派二号机",
-            "t": "回复测试",
-            "d": "2026-09-06",
-            "replyTo": "c-mtpvpnkk"
-        },
-        {
-            "id": "i-99",
-            "n": "杉果派三号机",
-            "t": "继续测试",
-            "d": "2026-09-06",
-            "replyTo": "i-98"
-        }
-    ]
-};
+window.SITE_CONFIG.comments = {};
 window.SITE_CONFIG.commentsEnabled = true;
