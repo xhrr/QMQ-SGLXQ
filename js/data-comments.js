9cc528fb737806949c7543afd4dca57475fa0eaa
Actor CMS <actor@localhost>
2026-09-06 14:01:17 +0000

    deploy: 2026-09-06T14:01:17.550Z

--- js/data-comments.js
+++ js/data-comments.js
@@ -1,3 +1,12 @@
 window.SITE_CONFIG = window.SITE_CONFIG || {};
-window.SITE_CONFIG.comments = {};
+window.SITE_CONFIG.comments = {
+    "album-a-IyyB3Q": [
+        {
+            "id": "c-mtpvpnkk",
+            "n": "杉果派",
+            "t": "测试评论：评论功能上线验证～",
+            "d": "2026-09-06"
+        }
+    ]
+};
 window.SITE_CONFIG.commentsEnabled = true;
