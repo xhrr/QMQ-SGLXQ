2e09819aaedc7ffcda60bbfb7e94a74a0db70650
Actor CMS <actor@localhost>
2026-09-07 06:33:27 +0000

    deploy: 2026-09-07T06:33:27.043Z

--- js/data-comments.js
+++ js/data-comments.js
@@ -1,3 +1,12 @@
 window.SITE_CONFIG = window.SITE_CONFIG || {};
-window.SITE_CONFIG.comments = {};
+window.SITE_CONFIG.comments = {
+    "work-w-Gd0DgQ": [
+        {
+            "id": "i-110",
+            "n": "大棚的瓜",
+            "t": "我来了！",
+            "d": "2026-09-07"
+        }
+    ]
+};
 window.SITE_CONFIG.commentsEnabled = true;
